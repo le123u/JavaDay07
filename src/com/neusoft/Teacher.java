bcb0b3be459e3c769dee2210f5411a914fb33d9d
le123u <63946085+le123u@users.noreply.github.com>
2020-05-31 14:33:19 +0800

    继承01

---
+++ src/com/neusoft/Teacher.java
@@ -0,0 +1,14 @@
+package com.neusoft;
+
+/**
+ * @author Liu
+ * @date 2020/5/31 14:27
+ */
+
+// 继承员工类
+public class Teacher extends Employee{
+    public void printName(){
+        System.out.println("name"+name);
+    }
+
+}
